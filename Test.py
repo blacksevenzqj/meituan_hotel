7bd2598ce6bd38998ec27d712912dee251544266
blacksevenzqj <397353721@qq.com>
2020-04-28 00:17:41 +0800

    1

--- Test.py
+++ Test.py
@@ -1,17 +1,49 @@
-import requests as curl, time
+#pip install --upgrade setuptools
+# https://blog.csdn.net/somezz/article/details/83104368
 
-rrr = "seven"
-print(rrr.encode())
-# print(rrr.encode('base64','strict'))
+import time, datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.executors.pool import ProcessPoolExecutor
 
-print(rrr.encode(encoding='UTF-8',errors='strict'))
+def func():
+    now = datetime.datetime.now()
+    ts = now.strftime('%Y-%m-%d %H:%M:%S')
+    print('do func time :',ts)
+ 
+def func2():
+    #耗时2S
+    now = datetime.datetime.now()
+    ts = now.strftime('%Y-%m-%d %H:%M:%S')
+    print('do func2 time：',ts)
+    time.sleep(2)
+ 
+def dojob():
 
-print(time.time())
+    scheduler = BlockingScheduler()
+    
+    #添加任务,时间间隔2S
+    scheduler.add_job(func, trigger='interval', seconds=2)
+    #添加任务,时间间隔5S
+    scheduler.add_job(func2, trigger='interval', seconds=3, id='test_job2')
+    scheduler.start()
+ 
+dojob()
 
 
-import getopt, sys
+# In[]:
+from datetime import datetime
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="]) # "ho:"也可以写成'-h-o:'
-print(opts)
-print(args)
+def tick():
+    print('Tick! The time is: %s' % datetime.now())
 
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(tick, 'interval', seconds=3)
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C    '))
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
